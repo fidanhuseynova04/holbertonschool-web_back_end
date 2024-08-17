9bc4773377c6b072d3ff2ff8715d934652187708
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-08-17 21:52:08 +0000

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that contains floor function
+"""
+
+
+def floor(a: float) -> int:
+    '''
+    Function returns the floor of the float
+    '''
+    return int(a)
