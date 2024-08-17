390bd042c35ecd8c3c114386ff8ce62214ef0b4b
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-08-17 21:53:41 +0000

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that contains to_str function
+"""
+
+
+def to_str(n: float) -> str:
+    '''
+    Function returns the str representation of the float
+    '''
+    return str(n)
